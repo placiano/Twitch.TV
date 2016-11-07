d504da5a50ad2dae7fcf31d5bb6e3b3569048e6a
Aleksandr Makarov <seems.deviant@gmail.com>
2016-11-08 00:13:37 +0200

    Looks like it works on a TV.

--- content/rcu_navigator.js
+++ content/rcu_navigator.js
@@ -1,19 +1,16 @@
 
 var rcuMenuItems = {
 	in_charge: 0,
-	dropped_out: 0,
 	idx: 0,
 	count: 0,
 	items: {},
-	invalidate: function() {
-		this.idx = -1;
-		this.count = 0;
-		this.items.empty();
-	},
+
 	update: function() {
 		this.items = $("div.left_panel_menu > ul > li > a,\
 				div.left_panel > div > input");
 		this.count = this.items.length;
+		this.idx = 0;
+		this.in_charge = 0;
 	},
 	navigate: function(direction) {
 		var dropped_out = 0;
@@ -30,7 +27,6 @@ var rcuMenuItems = {
 		case "right":
 			/* Release */
 			return -1;
-			//this.in_charge = 0;
 			break;
 		case "left":
 		case "none":
@@ -47,49 +43,70 @@ var rcuMenuItems = {
 
 var rcuGameItems = {
 	in_charge: 0,
-	dropped_out: 0,
 	idx: 0,
 	count: 0,
+	nr_rows : 0,
 	nr_items_in_row: 0,
 	items: {},
-	invalidate: function() {
-		this.idx = -1;
-		this.count = 0;
-		this.items.empty();
-	},
 	update: function() {
 		this.items = $("div.right_panel > div > div");
 		this.count = this.items.length;
+		this.idx = 0;
+		this.in_charge = 0;
 	},
-	calc_items_in_row: function() {
-		var x = $("#twitch-widget-gamelist");
-		var y = $("#twitch-widget-gamelist div").first();
-		this.nr_items_in_row = x.width() / y.outerWidth(true);
-		this.nr_items_in_row = Math.floor(this.nr_items_in_row);
+	calc_items_in_row: function(which_widget) {
+		var x = this.items.first().parent().width();
+		var y = this.items.first().outerWidth(true);
+
+		var h = this.items.first().parent().height();
+		var h2 = this.items.first().outerHeight(true);
+
+		var h3 = this.items.first().parent();
+		var h4 = this.items.first();
+		
+		this.nr_rows = Math.floor(h / h2);
+		this.nr_items_in_row = Math.floor(x / y);
+	},
+	is_leftmost: function(idx) {
+		return Boolean(!idx || 
+				!(idx % this.nr_items_in_row))
 	},
 	navigate: function(direction) {
-		var dropped_out = 0;
 
 		this.calc_items_in_row();
+
+		var leftmost = false, rightmost = false;
+
+		/* Possible situation when there is one item
+		* per row. */
+		if (!this.idx) {
+			leftmost = true;
+			if (this.nr_items_in_row == 1)
+				rightmost = leftmost;
+		} else
+			leftmost = (this.idx % this.nr_items_in_row) == 0;
+
+		/* It is not the first position in a single columnt layout.
+		* Maybe it is the rightmost position? */
+		if (!leftmost)
+			if (this.idx == this.count - 1)
+				rightmost = true;
+			else
+				rightmost = ((this.idx + 1) % this.nr_items_in_row) == 0;
+		
+
 		switch (direction) {
 		case "right":
-			if (!this.idx)
-				this.idx++;
 			/* I want the cursor run only to the end of the row. */
-			else if ((this.idx + 1) % this.nr_items_in_row > 0)
+			if (!rightmost)
 				this.idx++;
 			break;
 		case "left":
-			if (!this.idx)
-				return -1;
-				//this.in_charge = 0;
 			/* I do not want to jump to previous row. */
-			else if ((this.idx + 1) % (this.nr_items_in_row + 1) > 0)
+			if (!leftmost)
 				this.idx--;
 			else
-				//this.in_charge = 0;
 				return -1;
-
 			break;
 		case "up":
 			if ((this.idx - this.nr_items_in_row) >= 0)
@@ -112,6 +129,12 @@ var rcuGameItems = {
 };
 
 var rcuNavigator = {
+	update: function() {
+		if (rcuMenuItems.count == 0)
+			rcuMenuItems.update();
+		if (rcuGameItems.count >= 0)
+			rcuGameItems.update();
+	},
 	navigate: function(direction) {
 
 		var firstMove = Boolean((rcuGameItems.in_charge == 0) &&
--- content/twitch.js
+++ content/twitch.js
@@ -1,7 +1,7 @@
 var default_limit = 20;
 
 function defineScreen($title){
-	
+
 	$("#game-list-header").text($title);
 	$("#twitch-widget-gamelist").empty();
 	$("#twitch-widget-streamlist").empty();
@@ -10,9 +10,9 @@ function defineScreen($title){
 
 
 function searchChannel(search) {
-	
+
 	defineScreen("Search: " + search)
-	
+
 	  $.ajax({
           url: 'https://api.twitch.tv/kraken/search/streams?limit=100&q=' + search,
           type: 'GET',
@@ -24,34 +24,35 @@ function searchChannel(search) {
           success: function(data) {
 
           	$.each(data.streams, function(index, value){
-          		
+
           		channel_name = value.channel.name
 				channel_id = value.channel._id
 				channel_image = value.preview.medium
 				channgel_display_name = value.channel.display_name
 				channel_status = value.channel.status
 
-          		$("#twitch-widget-streamlist").append("<div class='stream_img'><a href='#' name='" + channel_name + "' id='" + channel_id + "'><img src='" + channel_image + "'></a><br><b>" + channgel_display_name + "</b><br/><div id='channel_status'>" + channel_status + "</div></div>");
-          		
+          		$("#twitch-widget-streamlist").append("<div class='stream_img' tabindex='-1'><a href='#' name='" + channel_name + "' id='" + channel_id + "'><img src='" + channel_image + "'></a><br><b>" + channgel_display_name + "</b><br/><div id='channel_status'>" + channel_status + "</div></div>");
+
           	})
+		rcuNavigator.update();
           }
 		});
 }
 
 function showGames(){
-	
+
 	defineScreen("All Games")
-	
+
 	 $.ajax({
 	          url: 'https://api.twitch.tv/kraken/games/top?limit=' + default_limit +'&offset=0',
 	          type: 'GET',
 		  data: {
 			client_id: '7zclzcxtiqcxfspf9ltnwztf8kvruwj'
 		  },
-		
+
 		  error: function(e) {
 			$("#twitch-error").append(e.status + ": " + e.statusText);
-		  }, 
+		  },
 	          contentType: 'application/json',
 	          dataType: 'jsonp',
 	          success: function(data) {
@@ -61,21 +62,20 @@ function showGames(){
 	          		game_name = value.game.name;
 	          		game_image = value.game.box.medium;
 	          		game_viewers = value.viewers;
-	          		
+
 	          		$("#twitch-widget-gamelist").append("<div class='game_item' name='" + game_name + "' id='" + game_id + "' tabindex='-1'><img src='" + game_image + "'><br><b>" + game_name + "</b><br/><div class='game_status'>" + game_viewers + " viewers</div></div>");
-	          	
+
 	          	})
-			rcuMenuItems.update();
-			rcuGameItems.update();
+			rcuNavigator.update();
 	          }
 	});
-			
+
 }
 
 function showChannels(){
-	
+
 	defineScreen("Channels")
-	
+
 	$.ajax({
 		url: 'https://api.twitch.tv/kraken/streams?limit=100',
 		type: 'GET',
@@ -85,18 +85,19 @@ function showChannels(){
 		contentType: 'application/json',
 		dataType: 'jsonp',
 		success: function(data) {
-	  	
+
 			$.each(data.streams, function(index, value){
-				
+
 				channel_name = value.channel.name
 				channel_id = value._id
 				channel_image = value.preview.medium
 				channel_display_name = value.channel.display_name
 				channel_viewers = value.viewers
 				channel_status = value.channel.status
-				
-				$("#twitch-widget-streamlist").append("<div class='stream_img'><a href='#' name='" + channel_name + "' id='" + channel_id + "'><img src='" + channel_image + "'></a><br><b>" + channel_display_name + " (" + channel_viewers + " viewers)</b><br/><div id='channel_status'>" + channel_status + "</div></div>");
+
+				$("#twitch-widget-streamlist").append("<div class='stream_img' tabindex='-1'><a href='#' name='" + channel_name + "' id='" + channel_id + "'><img src='" + channel_image + "'></a><br><b>" + channel_display_name + " (" + channel_viewers + " viewers)</b><br/><div id='channel_status'>" + channel_status + "</div></div>");
 			})
+			rcuNavigator.update();
 		}
 	});
 }
@@ -120,9 +121,10 @@ function showStreamers(game){
 
 		$.each(data.streams, function(index, value){
 
-		$("#twitch-widget-streamlist").append("<div class='stream_img'><a href='#' name='" + value.channel.name + "' id='" + value._id + "'><img src='" + value.preview.medium + "'></a><br><b>" + value.channel.status + "</b><br/><div class='game_status'>" + value.viewers + " viewers on " + value.channel.display_name + "</div></div>");
+		$("#twitch-widget-streamlist").append("<div class='stream_img' tabindex='-1'><a href='#' name='" + value.channel.name + "' id='" + value._id + "'><img src='" + value.preview.medium + "'></a><br><b>" + value.channel.status + "</b><br/><div class='game_status'>" + value.viewers + " viewers on " + value.channel.display_name + "</div></div>");
 
 		})
+		rcuNavigator.update();
 	}
 	});
 }
